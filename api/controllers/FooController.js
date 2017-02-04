d2b9db2bb9d8f53ffa53ce6cc2debd201e3a095c
Bianca N Hinojosa <biancanhinojosa@Biancas-MacBook-Pro.local>
2017-02-04 22:55:01 +0100

    implemented simple java parser

--- api/controllers/FooController.js
+++ api/controllers/FooController.js
@@ -5,7 +5,7 @@
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
 var GitHubApi = require("github");
-var esprima = require("esprima");
+var java_parser = require("java-parser");
 var ghdownload = require('github-download');
 var fs = require('fs');
 
@@ -18,14 +18,14 @@ module.exports = {
 
     console.log("Received create request");
     // console.log(esprima.tokenize()):
-    
+
     var now = new Date();
     var timeString = now.getTime();
 
     var deleteFolderRecursive = function(path) {
         if( fs.existsSync(path) ) {
             fs.readdirSync(path).forEach(function(file,index){
-            var curPath = path + "\\" + file;
+            var curPath = path + "/" + file;
             if(fs.lstatSync(curPath).isDirectory()) { // recurse
                 deleteFolderRecursive(curPath);
             } else { // delete file
@@ -38,13 +38,13 @@ module.exports = {
         }
     };
 
-    ghdownload({user: 'jprichardson', repo: 'node-batchflow', ref: 'master'}, process.cwd() + "/" + timeString)
+    ghdownload({user: 'CodeEtch', repo: 'backend', ref: 'master'}, process.cwd() + "/" + timeString)
     .on('error', function(err) {
         console.error(err)
     })
     .on('end', function() {
         // Should call our parser function
-        deleteFolderRecursive(process.cwd() + "\\" + timeString);
+        deleteFolderRecursive(process.cwd() + "/" + timeString);
     });
 
     console.log(process.cwd());
@@ -52,6 +52,40 @@ module.exports = {
     return res.json({
         desc: "Testing function"
     });
+  },
+  parse: function (req, res) {
+
+    var file = fs.readFileSync('test.java', 'utf8');
+    var syntax  = java_parser.parse(file);
+    var types = [];
+    for (var type of syntax.types) {
+      var functions = [];
+
+      for (var declaration of type.bodyDeclarations) {
+        var references = []
+
+        for (var statement of declaration.body.statements) {
+          references.push({
+            name:statement.expression.name.identifier
+          })
+        }
+
+        functions.push({
+          name:declaration.name,
+          references:references
+        })
+
+      }
+      types.push({
+        name:type.name,
+        functions:functions
+
+      })
+    }
+
+
+    return res.json(types);
+
   }
 };
 
---
+++ test.java
@@ -0,0 +1,13 @@
+ public class HelloWorld {
+
+     public static void main(String[] args) {
+         // Prints "Hello, World" to the terminal window.
+         System.out.println("Hello, World");
+         this.test1();
+     }
+
+     public void test1() {
+        System.out.println("Test");
+     }
+
+ }
